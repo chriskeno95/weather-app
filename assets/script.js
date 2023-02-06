db25407a9440802d12c6ff4e746c1a55e0df095c
chris kennedy <chris-kennedy09@hotmail.com>
2023-02-06 03:45:38 +0000

    added function to call users history from local storage and populate the page with clickable buttons that will also contain location value. added uppercase function as i could not find a way to capilatise just the first letter

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
 
+const userHistoryArr = localStorage.getItem("locations") ? JSON.parse(localStorage.getItem("locations")) : []
+
+console.log(userHistoryArr);
+
 var locationBtn = $(".location");
 var buttonStyle = "btn btn-primary m-1 rounded-pill location";
 var searchBtn = $(".search");
 var currentDate = $("#currentDate");
 var currentTime = $("#currentTime");
+var suggestions = $("#suggestions");
 
 
 window.setInterval(function () {
@@ -19,14 +24,37 @@ locationBtn.on("click", function() {
   alert("button clicked" + location)})
 
 
-  searchBtn.on("click", function() {
-  var searchedLocation = $("#search").val().trim();
-  localStorage.setItem(searchedLocation,searchedLocation);
-
-  alert("search location is" + searchedLocation);
-
-  //let newSuggestion = searchLocation;
-  //$("button").
+  searchBtn.on("click", function(event) {
+  event.preventDefault()
+  var searchedLocation = $("#search").val().trim().toUpperCase();
+  createLocation(searchedLocation)
+  //localStorage.setItem(searchedLocation,searchedLocation);
+  //alert("search location is " + searchedLocation);
+  $("#search").val('');
 })
 
+function displayHistory(){
+    let locations = "";
+    for(let i = 0; i < userHistoryArr.length; i++) 
+    //var newBtn = $("<button>").addClass(buttonStyle);
+    locations += `<button class="btn btn-primary m-1 rounded-pill location" "value="${userHistoryArr[i]}" type="submit">${userHistoryArr[i]}</button>`
+   // newBtn.text(userHistoryArr[i]);
+   suggestions.prepend(locations);
+}
+
+function createLocation(searchedLocation){
+    userHistoryArr.push(searchedLocation)
+    localStorage.setItem("locations", JSON.stringify(userHistoryArr))
+    location.reload()
+
+
+}
+
+//var newBtn = $("<button>").addClass(buttonStyle)
+//  newBtn.attr("value",searchedLocation);
+ // newBtn.text((localStorage.getItem("searchedLocation")))
+ // suggestions.prepend(newBtn);
+
+
+displayHistory()
   })
