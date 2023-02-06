b445bc6eacf998b77c9ab66a0fb995f304e96bd7
chris kennedy <chris-kennedy09@hotmail.com>
2023-02-06 15:18:14 +0000

    added functions to get coordinated of searched location/weather

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
 
+const APIKey = "b126ab1310c05c69b64388237ff59e49"
+var city;
+var weatherAPIURL = "http://api.openweathermap.org"
+//var weatherQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+//console.log(city);
 const userHistoryArr = localStorage.getItem("locations") ? JSON.parse(localStorage.getItem("locations")) : []
 
 console.log(userHistoryArr);
 
 var locationBtn = $(".location");
-var buttonStyle = "btn btn-primary m-1 rounded-pill location";
 var searchBtn = $(".search");
 var currentDate = $("#currentDate");
 var currentTime = $("#currentTime");
 var suggestions = $("#suggestions");
+var userSearch = $("#search").val().trim().toUpperCase()
 
 
 window.setInterval(function () {
@@ -18,19 +23,80 @@ window.setInterval(function () {
 }, 1000);
 
 
+function fetchWeather(location){
+    console.log(location);
+    let latitude = location.lat;
+    let longitude = location.lon;
+    console.log(latitude)
+    console.log(longitude)
+
+}
+
+function getCoordinates(city){
+    let coordinatesURL = `${weatherAPIURL}/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`;
+    //console.log(coordinatesURL);
+
+    $.ajax({
+    url: coordinatesURL,
+    method: "GET"
+  })
+    // After data comes back from the request
+    .then(function(response) {
+        if(!response[0]){
+            alert("location not found")
+        } else {
+            fetchWeather(response[0])
+        }
+      //console.log(response);
+      //var results = response.data
+      //console.log(results)
+  })
+}
+
+
+function displayLocationWeather(city){
+    let queryURL = weatherAPIURL + "/data/2.5/weather?q="
+
+    $.ajax({
+    url: queryURL + city + "&appid=" + APIKey,
+    method: "GET"
+  })
+    // After data comes back from the request
+    .then(function(response) {
+      console.log(response);
+      //var results = response.data
+      //console.log(results)
+  })
+   // console.log(city)
+}
 
 locationBtn.on("click", function() {
     let location = $(this).val();
-  alert("button clicked" + location)})
+  alert("button clicked " + location)})
 
 
   searchBtn.on("click", function(event) {
   event.preventDefault()
-  var searchedLocation = $("#search").val().trim().toUpperCase();
+  var searchedLocation = $("#search").val().trim().toUpperCase() || "";
+
+  if(searchedLocation === ""){
+    alert("please enter a city")
+    return
+  }
+
+  if(searchedLocation != ""){
+    city = searchedLocation
+console.log(city) 
+
+displayLocationWeather(city)
+getCoordinates(city)
+}
+
+
+
   createLocation(searchedLocation)
-  //localStorage.setItem(searchedLocation,searchedLocation);
-  //alert("search location is " + searchedLocation);
   $("#search").val('');
+  return city
 })
 
 function displayHistory(){
@@ -43,12 +109,23 @@ function displayHistory(){
 }
 
 function createLocation(searchedLocation){
+    if(userHistoryArr.indexOf(searchedLocation) !== -1){
+        return;
+    }
     userHistoryArr.push(searchedLocation)
     localStorage.setItem("locations", JSON.stringify(userHistoryArr))
-    location.reload()
-
-
+    //location.reload()
 }
+
+
+
+
+
+
+
+
+
+
 
 //var newBtn = $("<button>").addClass(buttonStyle)
 //  newBtn.attr("value",searchedLocation);
